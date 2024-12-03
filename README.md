e50d36fde8069e6979cec74d0ecd65b96df20bee
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2024-12-03 05:48:55 +0545

    docs: update README and refactor PDF response functions

--- README.md
+++ README.md
@@ -9,7 +9,18 @@ bun install
 To run:
 
 ```bash
-bun run index.ts
+bun dev
+
+
+```
+For running unit test
+```sh 
+bun test
+```
+
+To run browser test
+```sh 
+bun test:puppet
 ```
 
 This project was created using `bun init` in bun v1.1.37. [Bun](https://bun.sh) is a fast all-in-one JavaScript runtime.
--- api/core/core.ts
+++ api/core/core.ts
@@ -1,22 +1,27 @@
-export async function downloadPDF() {
-	// const cloud = require("@sparticuz/chromium");
-	// // biome-ignore lint/style/useSingleVarDeclarator: <explanation>
-	// const executablePath = await cloud.executablePath()
-	const { chromium } = require("playwright-core");
-
-	return { c: chromium };
-}
+import * as buffer from "node:buffer";
 
 export const cloud = require("chrome-aws-lambda");
 
-export function pdfResponse(pdf: Buffer, fileName: string) {
+export function base64EncodedResponse(base64Encoded: Buffer, fileName) {
 	const response = {
 		headers: {
 			"Content-type": "application/pdf",
 			"content-disposition": `attachment; filename=${fileName}.pdf`,
 		},
 		statusCode: 200,
-		body: pdf.toString("base64"),
+		body: buffer.toString("base64"),
 		isBase64Encoded: true,
 	};
+}
+export function pdfResponse(pdf: Buffer, fileName: string) {
+	return base64EncodedResponse();
+}
+
+export async function downloadPDF() {
+	// const cloud = require("@sparticuz/chromium");
+	// // biome-ignore lint/style/useSingleVarDeclarator: <explanation>
+	// const executablePath = await cloud.executablePath()
+	// const { chromium } = require("playwright-core");
+
+	return pdfResponse(null, "portfolio.pdf");
 }
