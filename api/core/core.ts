6a777ead8a252184e968805745cf883ab3fc6ca3
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2024-12-03 05:56:20 +0545

    refactor(core): clean up unused code and update dependencies

--- api/core/core.ts
+++ api/core/core.ts
@@ -1,27 +1,38 @@
-import * as buffer from "node:buffer";
-
-export const cloud = require("chrome-aws-lambda");
-
-export function base64EncodedResponse(base64Encoded: Buffer, fileName) {
-	const response = {
-		headers: {
-			"Content-type": "application/pdf",
-			"content-disposition": `attachment; filename=${fileName}.pdf`,
-		},
-		statusCode: 200,
-		body: buffer.toString("base64"),
-		isBase64Encoded: true,
-	};
-}
-export function pdfResponse(pdf: Buffer, fileName: string) {
-	return base64EncodedResponse();
-}
+// import * as buffer from "node:buffer";
+//
+// export const cloud = require("chrome-aws-lambda");
+//
+// export function base64EncodedResponse(base64Encoded: ArrayBuffer, fileName) {
+// 	const response = {
+// 		headers: {
+// 			"Content-type": "application/pdf",
+// 			"content-disposition": `attachment; filename=${fileName}.pdf`,
+// 		},
+// 		statusCode: 200,
+// 		body: buffer.toString("base64"),
+// 		isBase64Encoded: true,
+// 	};
+// 	return response
+// }
+// export function pdfResponse(pdf: Buffer, fileName: string) {
+// 	return base64EncodedResponse(pdf, fileName);
+// }
 
 export async function downloadPDF() {
-	// const cloud = require("@sparticuz/chromium");
-	// // biome-ignore lint/style/useSingleVarDeclarator: <explanation>
-	// const executablePath = await cloud.executablePath()
-	// const { chromium } = require("playwright-core");
+	const cloud = require("@sparticuz/chromium");
+	const executablePath = await cloud.executablePath();
+	const { chromium } = require("playwright-core");
+	const browser = await chromium.launch({ headless: false });
+	const page = await browser.newPage();
+	let url: string;
+	url = "https://ax-sh.github.io/";
+	await page.goto(url);
+	// await page.close()
+	// await browser.close()
+	return { args: cloud.args, executablePath };
 
-	return pdfResponse(null, "portfolio.pdf");
+	//
+	// console.log(	chromium.args)
+	//
+	// return pdfResponse(null, "portfolio.pdf");
 }
